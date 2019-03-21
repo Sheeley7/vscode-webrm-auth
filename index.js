ec315aca699259f8050a36b5b78acc6f3e68d4e1
Isaac Sheeley <Sheel7@users.noreply.github.com>
2019-03-21 00:10:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -76,53 +76,16 @@ app.get('/callback', function (req, res) {
           message = 'error: ' + err.message + '\n';
         }
         message += 'response: ' + JSON.stringify(response);
-
-        if (err) {
-          res.send(message);
-          return;
-        }
-
-        // Later, if the access token is expired it can be refreshed.
-        authenticationContext.acquireTokenWithRefreshToken(response.refreshToken, clientId, clientSecret, resource, function(refreshErr, refreshResponse) {
-          if (refreshErr) {
-            message += 'refreshError: ' + refreshErr.message + '\n';
-          }
-          message += 'refreshResponse: ' + JSON.stringify(refreshResponse);
-          
-          res.send(message); 
-        }); 
-    });
-    
-
-    
+        res.redirect('https://webresourcemanagerauth.azurewebsites.net/result?' +
+        querystring.stringify({
+            token: response.accessToken
+        })); 
+        
+    });    
 });
 
 app.get('/result', function (req, res) {
-    res.send("DONE");
-});
-
-app.get('/refresh_token', function (req, res) {
-
-    // requesting access token from refresh token
-    const refresh_token = req.query.refresh_token;
-    const authOptions = {
-        url: 'https://login.microsoftonline.com/common/oauth2/token',
-        headers: { 'Content-Type': "application/x-www-form-urlencoded" },
-        form: {
-            grant_type: 'refresh_token',
-            refresh_token: refresh_token
-        },
-        json: true
-    };
-
-    request.post(authOptions, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-            const access_token = body.access_token;
-            res.send({
-                'access_token': access_token
-            });
-        }
-    });
+    res.send(req.query.code);
 });
 
 app.listen(process.env.PORT || 3000);
