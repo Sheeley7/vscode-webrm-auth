5e448250336dfd434fd9b21b9ca1c2c9073c8348
sheel7 <sheeley7@gmail.com>
2019-03-24 12:12:19 -0400

    test

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.CLIENT_SECRET;
 const redirect_uri = "https://wrm-test1.azurewebsites.net/"//process.env.SPOTIFY_REDIRECT_URI;
 const final_redirect_uri = process.env.FINAL_REDIRECT_URI;
-const crm_url;
+var crm_url;
 
 /**
  * Generates a random string
@@ -35,7 +35,7 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/login', function (req, res) {
 
-    crm_url = req.query.crm_url;
+    //crm_url = req.query.crm_url;
 
     const state = generateRandomString(16);
     res.cookie(stateKey, state);
@@ -49,7 +49,7 @@ app.get('/login', function (req, res) {
             prompt: "consent",
             state: state
         }));*/
-    res.send(client_id + "\n" + crm_url);
+    res.send(client_id + "\n");
     
 });
 
